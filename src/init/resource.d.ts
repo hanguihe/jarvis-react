f7ac8fe02ca49f80acba917f7c816f2ca1224653
hanguihe <yunbie.han@tuya.com>
2021-06-25 10:21:51 +0800

    feat: 更新文件结构

--- src/init/resource.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const configurationInfo: Map<string, {
-    description: string;
-    needDepend: string[];
-}>;
-export declare const scripts: {
-    prettier: string;
-    'style-fix': string;
-    'eslint-fix': string;
-};
--- src/webpack/config.d.ts
+++
@@ -1,2 +0,0 @@
-import { Configuration } from 'webpack';
-export default function (env: 'production' | 'development'): Configuration;
